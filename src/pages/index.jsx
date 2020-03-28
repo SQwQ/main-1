b389fbd1563cba61998febe6aff325056381016b
PhuaYK <44082510+PhuaYK@users.noreply.github.com>
2020-03-28 18:30:27 +0800

    Changed Linking

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,32 +1,63 @@
 import React from 'react';
-import { Button } from '@material-ui/core';
-import './css/index.css' 
-import UserRegisterForm from '../components/RegistrationForms/UserRegisterForm'
-import RiderRegisterForm from '../components/RegistrationForms/RiderRegisterForm'
-import StaffRegisterForm from '../components/RegistrationForms/StaffRegisterForm'
+import {Button} from '@material-ui/core';
+import './css/index.css';
+import UserRegisterForm from '../components/RegistrationForms/UserRegisterForm';
+import RiderRegisterForm
+  from '../components/RegistrationForms/RiderRegisterForm';
+import StaffRegisterForm
+  from '../components/RegistrationForms/StaffRegisterForm';
 
 import {Link as RouterLink} from 'react-router-dom';
 
-const MainPage = () => {
+const handleLink = (reroute, id) => {
+  this.props.history.push ({
+    pathname: `/${reroute}/` + id,
+  });
+};
 
-    return (
-        <div class="MainPageBody">
-            <h1>tapau.</h1>
-            <h3>Main Page</h3>
-            <div>        
-                <Button variant="contained" color="primary" component={RouterLink} to="/user">Customer</Button>
-                <Button variant="contained" color="primary" component={RouterLink} to="/rider">Delivery Rider</Button>
-                <Button variant="contained" color="primary" component={RouterLink} to="/staff">Restaurant Staff</Button>
-                <Button variant="contained" color="primary" component={RouterLink} to="/manager">FDS Manager</Button>
-            </div>
-            <br/><br/>
-            <div>
-                <UserRegisterForm />
-                <RiderRegisterForm />
-                <StaffRegisterForm />
-            </div>
-        </div>
-    );
-}
+const MainPage = () => {
+  return (
+    <div class="MainPageBody">
+      <h1>tapau.</h1>
+      <h3>Main Page</h3>
+      <div>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => handleLink ('user')}
+        >
+          Customer
+        </Button>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => handleLink ('rider')}
+        >
+          Delivery Rider
+        </Button>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => handleLink ('staff')}
+        >
+          Restaurant Staff
+        </Button>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => handleLink ('manager')}
+        >
+          FDS Manager
+        </Button>
+      </div>
+      <br /><br />
+      <div>
+        <UserRegisterForm />
+        <RiderRegisterForm />
+        <StaffRegisterForm />
+      </div>
+    </div>
+  );
+};
 
 export default MainPage;
