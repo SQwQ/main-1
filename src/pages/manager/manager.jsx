af91255a020c6d2dc5042a25e2afe5c31a991842
PhuaYK <44082510+PhuaYK@users.noreply.github.com>
2020-03-28 17:58:50 +0800

    Concurrently!

--- src/pages/manager/manager.jsx
+++ src/pages/manager/manager.jsx
@@ -1,9 +1,11 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import ManagerNavBar from './navbar.jsx';
 
 export class ManagerPage extends Component {
     render() {
         return (
             <div>
+                <ManagerNavBar/>
                 Manager Page
             </div>
         );
