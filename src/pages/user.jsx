de6e1a81f639b298a7c431263a42c1048d596ad3
Q-gabe <gabriel.ong@u.nus.edu>
2020-03-30 17:03:22 +0800

    add user data fetch on user page && refactor to class component

--- src/pages/user.jsx
+++ src/pages/user.jsx
@@ -1,29 +1,36 @@
-//import React, { Component } from 'react';
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import React, { Component } from 'react';
+import { withStyles } from '@material-ui/core/styles';
+
+// UI Components
 import Drawer from '@material-ui/core/Drawer';
-import CssBaseline from '@material-ui/core/CssBaseline';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import List from '@material-ui/core/List';
 import Typography from '@material-ui/core/Typography';
 import Divider from '@material-ui/core/Divider';
 import ListItem from '@material-ui/core/ListItem';
+
+// Icons and Graphics
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import MailIcon from '@material-ui/icons/Mail';
-import banner from '../images/food.jpeg';
 import ProfileIcon from '@material-ui/icons/AccountCircle';
 import MonetizationOnIcon from '@material-ui/icons/MonetizationOn';
 import LogoutIcon from '@material-ui/icons/ExitToApp';
+import banner from '../images/food.jpeg';
+
+// Styles
+import CssBaseline from '@material-ui/core/CssBaseline';
 import './css/user.css';
 
-import {Link as RouterLink, useParams} from 'react-router-dom';
+// Routing
+import {Link as RouterLink, withRouter} from 'react-router-dom';
+import * as apiRoute from '../components/Api/route.js';
+import Axios from 'axios';
 
 const drawerWidth = 240;
-// const samplePoints = 999;
 
-const useStyles = makeStyles(theme => ({
+const styles = theme => ({
   root: {
     display: 'flex',
   },
@@ -45,68 +52,110 @@ const useStyles = makeStyles(theme => ({
     backgroundColor: theme.palette.background.default,
     padding: theme.spacing(0,0),
   },
-}));
+});
 
-export default function PermanentDrawerLeft(props) {
-  const classes = useStyles();
-  let { id } = useParams();
-  
-  return (
-    <div className={classes.root}>
-      <CssBaseline />
-      <AppBar position="fixed" className={classes.appBar}>
-        <Toolbar>
-          <Typography variant="h6" noWrap>
-            Hungry, User {id}? Have your meal delivered to you.
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <Drawer
-        className={classes.drawer}
-        variant="permanent"
-        classes={{
-          paper: classes.drawerPaper,
-        }}
-        anchor="left"
-      >
-        <div className={classes.toolbar} />
-        <Divider />
-        <List>
-            <ListItem button key="Profile">
-                <ListItemIcon><ProfileIcon /></ListItemIcon>
-                <ListItemText primary="Profile" />
-            </ListItem>
-            <ListItem button key="Vouchers">
-                <ListItemIcon><MailIcon /></ListItemIcon>
-                <ListItemText primary="Vouchers" />
-            </ListItem>
-        </List>
-        <Divider />
-        <List>
-            <ListItem key="Reward_Points">
-                <ListItemIcon><MonetizationOnIcon /></ListItemIcon>
-                <ListItemText primary="Points" />
-            </ListItem>
-            <ListItem button key="Logout" component={RouterLink} to="/"
-                onClick={props.unauthenticate}
+
+class UserPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+        this.id = this.props.match.params.id;
+        this.fetchUserData();
+    }
+
+    // Fetch user information upon login
+    fetchUserData() {
+        Axios.get(apiRoute.CUSTOMER_API + "/" + this.id, {
+            withCredentials: false,
+        })
+            .then (
+                response => {
+                    let { cname, 
+                        ccontact_number, 
+                        crewards_points,
+                        cusername,
+                        cpassword,
+                        cjoin_time } = response.data
+                    
+                    // Add user details to state
+                    this.setState({
+                        cname: cname,
+                        ccontact_number: ccontact_number,
+                        crewards_points: crewards_points,
+                        cusername: cusername,
+                        cpassword: cpassword,
+                        cjoin_time: cjoin_time
+                    }) 
+                    
+                }
+            ).catch (error => {
+                console.log("Error getting customer details!");
+                console.log(error);
+            });
+    }
+
+    render() {
+        const { classes }= this.props;
+
+        return (
+            <div className={classes.root}>
+            <CssBaseline />
+            <AppBar position="fixed" className={classes.appBar}>
+                <Toolbar>
+                <Typography variant="h6" noWrap>
+                    {/* Conditional Title */}
+                    Hungry, {this.state.cname}? Have your meal delivered to you.
+                </Typography>
+                </Toolbar>
+            </AppBar>
+            <Drawer
+                className={classes.drawer}
+                variant="permanent"
+                classes={{
+                paper: classes.drawerPaper,
+                }}
+                anchor="left"
             >
-                <ListItemIcon><LogoutIcon /></ListItemIcon>
-                <ListItemText primary="Logout"/>
-            </ListItem>
-        </List>
-      </Drawer>
-      <main className={classes.content}>
-        <div className={classes.toolbar} />
-        <img className="banner" src={banner} alt="banner" />
-        <div className="mainContent">
-            <Typography variant="h2">
-            Hungry?
-            </Typography>
-            {/* Search Bar */}
+                <div className={classes.toolbar} />
+                <Divider />
+                <List>
+                    <ListItem button key="Profile">
+                        <ListItemIcon><ProfileIcon /></ListItemIcon>
+                        <ListItemText primary="Profile" />
+                    </ListItem>
+                    <ListItem button key="Vouchers">
+                        <ListItemIcon><MailIcon /></ListItemIcon>
+                        <ListItemText primary="Vouchers" />
+                    </ListItem>
+                </List>
+                <Divider />
+                <List>
+                    <ListItem key="Reward_Points">
+                        <ListItemIcon><MonetizationOnIcon /></ListItemIcon>
+                        <ListItemText primary={this.state.crewards_points + " Reward Point(s)"} />
+                    </ListItem>
+                    <ListItem button key="Logout" component={RouterLink} to="/"
+                        onClick={this.props.unauthenticate}
+                    >
+                        <ListItemIcon><LogoutIcon /></ListItemIcon>
+                        <ListItemText primary="Logout"/>
+                    </ListItem>
+                </List>
+            </Drawer>
+            <main className={classes.content}>
+                <div className={classes.toolbar} />
+                {/* Conditional iframe */}
+                {/* ORDER PAGE */}
+                <img className="banner" src={banner} alt="banner" />
+                <div className="mainContent">
+                    {/* Search Bar */}
 
-            {/* Card List */}
-        </div>
-      </main>
-    </div>
-  );
+                    {/* Card List */}
+                </div>
+            </main>
+            </div>
+        );
+    }
 }
+
+export default withStyles(styles, { withTheme: true })(withRouter(UserPage));
