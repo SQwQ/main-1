dce64983ae4c57bb73aa81c3251126a7f1454bfc
Francis <francislow_6@hotmail.com>
2020-03-30 16:50:34 +0800

    Implement backend updating of promotion/coupon/campaign

--- server/routes/promotions/promotionRoutes.js
+++ server/routes/promotions/promotionRoutes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const pool = require('../../config/pool');
 
-//Create a promotion
+// Create a promotion
 router.route('/api/promotion/new/:type').post((req, res) => {
   const type = req.params.type;
   const pisPercentage = req.body.pisPercentage;
@@ -19,9 +19,7 @@ router.route('/api/promotion/new/:type').post((req, res) => {
         '${pname}', '${pdescription}') returning pid;`
     )
     .then(returnData => {
-      console.log('ran');
       if (type == 'coupon') {
-        console.log('ran coupon');
         const couponCode = req.body.couponCode;
         pool
           .query(
@@ -30,7 +28,6 @@ router.route('/api/promotion/new/:type').post((req, res) => {
           )
           .catch(err => res.status(400).json('Error' + err));
       } else if (type == 'campaign') {
-        console.log('ran campaign');
         const cMon = req.body.cMon;
         const cTue = req.body.cTue;
         const cWed = req.body.cWed;
@@ -50,7 +47,7 @@ router.route('/api/promotion/new/:type').post((req, res) => {
     .catch(err => res.status(400).json('Error' + err));
 });
 
-//Delete a promotion
+// Delete a promotion
 router.route('/api/promotion/delete/:type/:pid').delete((req, res) => {
   const pid = req.params.pid;
   const type = req.params.type;
@@ -78,11 +75,28 @@ router.route('/api/promotion/delete/:type/:pid').delete((req, res) => {
   }
 });
 
+// Get a Promotion (including coupon/campaign details)
+router.route('/api/promotion/:type/:pid').get((req, res) => {
+  const pid = req.params.pid;
+  const type = req.params.type;
+  const type_table_name = type == 'coupon' ? 'Coupon' : 'Campaign';
+
+  pool
+    .query(
+      `SELECT * FROM Promotion as P
+       LEFT JOIN ${type_table_name} as T ON P.pid = T.pid
+       WHERE P.pid = ${pid};`
+    )
+    .then(result => {
+      res.send(JSON.stringify(result.rows));
+      res.status(204).json();
+    })
+    .catch(err => res.status(400).json('Error' + err));
+});
+
 // Update a Promotion
 router.route('/api/promotion/update/:pid').patch((req, res) => {
   const pid = req.params.pid;
-  console.log('pid', pid);
-
   const pisPercentage = req.body.pisPercentage;
   const pdatetime_active_from = req.body.pdatetime_active_from;
   const pdatetime_active_to = req.body.pdatetime_active_to;
@@ -90,23 +104,52 @@ router.route('/api/promotion/update/:pid').patch((req, res) => {
   const pdiscount_val = req.body.pdiscount_val;
   const pname = req.body.pname;
   const pdescription = req.body.pdescription;
-  console.log('pname', pname);
 
   pool
     .query(
       `UPDATE Promotion
-         SET pisPercentage=${pisPercentage}, pdatetime_active_from=${pdatetime_active_from}, pdatetime_active_to=${pdatetime_active_to}, 
-         pminSpend=${pminSpend}, pdiscount_val=${pdiscount_val}, pname='${pname}', pdescription='${pdescription}'
-         WHERE pid = ${pid};`
+       SET pisPercentage=${pisPercentage}, pdatetime_active_from=${pdatetime_active_from}, pdatetime_active_to=${pdatetime_active_to}, 
+       pminSpend=${pminSpend}, pdiscount_val=${pdiscount_val}, pname='${pname}', pdescription='${pdescription}'
+       WHERE pid = ${pid};`
     )
     .then(res.status(204).json())
     .catch(err => res.status(400).json('Error' + err));
 });
 
-// Update a Coupon
+// Update a Coupon (coupon code)
+router.route('/api/coupon/update/:pid').patch((req, res) => {
+  const pid = req.params.pid;
+  const couponCode = req.body.couponCode;
 
-// Update a Campaign
+  pool
+    .query(
+      `UPDATE Coupon
+       SET couponCode='${couponCode}'
+       WHERE pid = ${pid};`
+    )
+    .then(res.status(204).json())
+    .catch(err => res.status(400).json('Error' + err));
+});
 
-// Get a Promotion
+// Update a Campaign (days)
+router.route('/api/campaign/update/:pid').patch((req, res) => {
+  const pid = req.params.pid;
+  const cMon = req.body.cMon;
+  const cTue = req.body.cTue;
+  const cWed = req.body.cWed;
+  const cThu = req.body.cThu;
+  const cFri = req.body.cFri;
+  const cSat = req.body.cSat;
+  const cSun = req.body.cSun;
+
+  pool
+    .query(
+      `UPDATE Campaign
+       SET cMon=${cMon}, cTue=${cTue}, cWed=${cWed}, cThu=${cThu}, cFri=${cFri}, cSat=${cSat}, cSun=${cSun}
+       WHERE pid = ${pid};`
+    )
+    .then(res.status(204).json())
+    .catch(err => res.status(400).json('Error' + err));
+});
 
 module.exports = router;
