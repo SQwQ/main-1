b010bcd62ea827d92d58d061e7e666c52c675033
SQwQ <33057619+SQwQ@users.noreply.github.com>
2020-05-01 23:59:19 +0800

    。

--- projectDDL(update here).sql
+++ projectDDL(update here).sql
@@ -50,6 +50,7 @@ CREATE TABLE food_categorized (
 */
 CREATE TABLE Order_List (
 	ocid SERIAL NOT NULL PRIMARY KEY,
+	rid SERIAL,
 	oorder_place_time TIMESTAMP,
 	oorder_enroute_restaurant TIMESTAMP,
 	oorder_arrives_restaurant TIMESTAMP,
@@ -59,7 +60,8 @@ CREATE TABLE Order_List (
 	ofinal_price NUMERIC NOT NULL CHECK (ofinal_price >= 0),
 	opayment_type TEXT,
 	orating INTEGER,
-	ostatus TEXT
+	ostatus TEXT,
+	FOREIGN KEY (rid) REFERENCES Rider(rid)
 );
 
 CREATE TABLE order_contains (
@@ -83,13 +85,13 @@ CREATE TABLE Customer (
 );
 
 CREATE TABLE make_order (
-	rest_rating INTEGER,
+	dlvry_rating INTEGER,
 	review_text TEXT,
 	ocid SERIAL NOT NULL UNIQUE,
 	rid SERIAL NOT NULL,
 	cid SERIAL NOT NULL,
 	FOREIGN KEY (ocid) REFERENCES Order_List(ocid),
-	FOREIGN KEY (rid) REFERENCES Restaurant(rid),
+	FOREIGN KEY (rid) REFERENCES Rider(rid),
 	FOREIGN KEY (cid) REFERENCES Customer(cid)
 );
 
