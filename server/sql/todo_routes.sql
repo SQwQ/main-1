82e7d0c532a58850e287f8c5028928c5124f06ed
SQwQ <33057619+SQwQ@users.noreply.github.com>
2020-05-04 02:32:07 +0800

    Mostly complete set of queries

--- server/sql/todo_routes.sql
+++ server/sql/todo_routes.sql
@@ -1,4 +1,32 @@
-/* Update/Delete routes for rider/customre/staff
+/* Make an Order */
+/* 1) Insert into Order_List (ofinal_price = 0)
+   2) Insert into make_order 
+   3) Insert into order_contains (trigger on order_contains will auto discount food prices if promo is applicable) */
+
+/* Update final order price */
+   UPDATE Order_List SET ofinal_price = (SELECT SUM(total_price) FROM order_contains WHERE ocid = 'ocid') WHERE ocid = 'ocid';
+
+/* Apply restaurant percentage discount if applicable */
+   UPDATE Order_List SET ofinal_price = ofinal_price * 
+   (1 - (SELECT SUM(pisPercentage)/100 FROM Promotion WHERE prid = 'restaurant_id' 
+   AND CURRENT_TIMESTAMP >= pdatetime_active_from 
+	 AND CURRENT_TIMESTAMP <= pdatetime_active_to)) WHERE ocid = 'ocid';
+
+/* Apply Coupons if any. Loop the queries until all the coupons the customer want to use is used up */
+   UPDATE Order_List SET ofinal_price = ofinal_price - 
+   (SELECT pdiscount_val FROM Promotion WHERE pid = 'coupon_id' 
+   AND CURRENT_TIMESTAMP >= pdatetime_active_from 
+	 AND CURRENT_TIMESTAMP <= pdatetime_active_to)) WHERE ocid = 'ocid';
+
+   DELETE FROM Coupon WHERE couponCode = 'coupon_code';
+
+/* Give customers reward points */
+  UPDATE Customers SET crewards_points =  crewards_points + 1 WHERE cid = 'cid'; 
+
+
+/* Update Base salary for riders */
+UPDATE Full_Timers SET base_salary = x; /* trigger auto updates rider/ mthly past salary table. */
+UPDATE Part_Timers SET base_salary = x; /* Same for part timer */
 
 /*
 *Support data access for the different users (e.g., customers could view review postings and their past orders,
@@ -51,16 +79,15 @@ include the following:*/
 /*For each month, the total number of new customers, the total number of orders, and the total
 cost of all orders.*/
 
-SELECT new_cust_num as
-(SELECT COUNT(cid) 
+SELECT COUNT(cid) 
 FROM Customer c
 WHERE c.join_time >= CURRENT_TIMESTAMP - INTERVAL'28 days' 
-AND c.cid IN 
-(SELECT cid FROM Order_List WHERE rid = 'rid')), 
+AND c.cid IN (SELECT cid FROM make_order);
+
+SELECT
 COUNT(oid), SUM(ofinal_price)
 FROM Order_List 
-WHERE oorder_place_time >= CURRENT_TIMESTAMP - INTERVAL '28 days'
-AND rid = 'rid'
+WHERE oorder_arrives_customer >= CURRENT_TIMESTAMP - INTERVAL '28 days';
  
 
 /*For each month and for each customer who has placed some order for that month,
@@ -68,37 +95,41 @@ the total number of orders placed by the customer for that month and the total c
 these orders.*/
 
 /* total no. of orders by customer per mth */
-SELECT COUNT(oid) FROM order_contains WHERE ocid IN
-(Select o.ocid FROM 
-Order_List o,  
-WHERE o.oorder_arrives_customer >= CURRENT_TIMESTAMP - INTERVAL '28 days' 
-AND o.cid = 'cid')
+SELECT COUNT(ocid) FROM make_order WHERE cid = 'cid' AND ocid IN
+(Select ocid FROM 
+Order_List,  
+WHERE oorder_arrives_customer >= CURRENT_TIMESTAMP - INTERVAL '28 days' 
+);
 
 /* total cost of all orders by customer per mth */
-SELECT SUM(total_price) FROM order_contains WHERE ocid IN
-(Select o.ocid FROM 
-Order_List o,  
-WHERE o.oorder_arrives_customer >= CURRENT_TIMESTAMP - INTERVAL '28 days' 
-AND o.cid = 'cid')
+SELECT SUM(ofinal_price) FROM Order_List 
+WHERE oorder_arrives_customer >= CURRENT_TIMESTAMP - INTERVAL '28 days' AND 
+ocid IN
+(SELECT ocid FROM make_order WHERE cid = 'cid')
 
+/* For restaurant staff */
 
 /*For each month, the total number of completed orders, the total cost of all completed orders
 (excluding delivery fees), and the top 5 favorite food items (in terms of the number of orders
 for that item).*/
 
-/* total num of orders per mth */
-SELECT COUNT(oid) 
-FROM Order_List WHERE oorder_place_time >= CURRENT_TIMESTAMP - INTERVAL '28 days' ;
+/* total num of orders per mth/restaurant */
+SELECT COUNT(ocid) 
+FROM make_order WHERE rid = 'rid' AND ocid IN 
+(SELECT ocid FROM Order_List WHERE oorder_arrives_customer >= CURRENT_TIMESTAMP - INTERVAL '28 days') ;
 
 /* total cost of all completed orders per mth */
 SELECT SUM(ofinal_price)
-FROM Order_List WHERE oorder_place_time >= CURRENT_TIMESTAMP - INTERVAL '28 days' ;
+FROM Order_List WHERE oorder_arrives_customer >= CURRENT_TIMESTAMP - INTERVAL '28 days' 
+AND ocid IN (SELECT ocid FROM make_order WHERE rid = 'rid');
 
 /* Top 5 fav food */
 SELECT fid
 FROM 
 (SELECT fid, COUNT(fid) as Order_num
-FROM order_contains WHERE oorder_place_time >= CURRENT_TIMESTAMP - INTERVAL '28 days')
+FROM order_contains WHERE ocid IN 
+(SELECT ocid FROM make_order WHERE rid = 'rid' AND ocid IN 
+(SELECT ocid FROM Order_List WHERE oorder_arrives_customer >= CURRENT_TIMESTAMP - INTERVAL '28 days')))
 ORDER BY Order_num  
 LIMIT 5
 
@@ -109,47 +140,48 @@ of ratings received by the rider for all the orders delivered for that month, an
 rating received by the rider for all the orders delivered for that month.*/
 
 /* total number of orders delivered per mth */
-SELECT COUNT(ocid) FROM Order_List WHERE rid = 1 AND oorder_arrives_customer >= CURRENT_TIMESTAMP - INTERVAL '28 days';
+SELECT COUNT(rid) FROM delivered_by WHERE rid = 1 AND ocid IN 
+(SELECT ocid FROM Order_List WHERE oorder_arrives_customer >= CURRENT_TIMESTAMP - INTERVAL '28 days');
 
 /* the total number of hours worked by the rider for that month */
 SELECT COUNT(DISTINCT start_time, wkday)  FROM Schedule_Count WHERE scid IN 
 (SELECT scid FROM Current_Schedule WHERE rid = 1);
 
-/* total monthly salary */
-SELECT rtotal_salary FROM Rider WHERE rid = 1;
+/* total full time monthly salary */
+SELECT rtotal_salary FROM rider WHERE rid = 1;
 
 /* average delivery time per mth */
 SELECT SUM(oorder_arrives_customer - oorder_place_time)/COUNT(ocid) FROM Order_List WHERE ocid IN 
-(SELECT ocid FROM Order_List WHERE rid = 1 AND oorder_arrives_customer >= CURRENT_TIMESTAMP - INTERVAL '28 days');
+(SELECT ocid FROM delivered_by WHERE rid = 1 AND ocid IN 
+(SELECT ocid FROM Order_List WHERE oorder_arrives_customer >= CURRENT_TIMESTAMP - INTERVAL '28 days'));
 
 
 /* the number of ratings received by the rider per mth*/
-SELECT COUNT(ocid) FROM make_order WHERE ocid IN 
-(SELECT ocid FROM Order_List WHERE rid = 1 AND oorder_arrives_customer >= CURRENT_TIMESTAMP - INTERVAL '28 days');
+SELECT COUNT(ocid) FROM delivered_by WHERE rid = 1 AND ocid IN 
+(SELECT ocid FROM Order_List WHERE oorder_arrives_customer >= CURRENT_TIMESTAMP - INTERVAL '28 days');
 
 /* the average rating received by the rider per mth */
-SELECT SUM(dlvry_rating)/COUNT(ocid) FROM make_order WHERE ocid IN 
-(SELECT ocid FROM Order_List WHERE rid = 1 AND oorder_arrives_customer >= CURRENT_TIMESTAMP - INTERVAL '28 days');
+SELECT SUM(drating)/COUNT(ocid) FROM delivered_by WHERE rid = 1 AND ocid IN 
+(SELECT ocid FROM Order_List WHERE oorder_arrives_customer >= CURRENT_TIMESTAMP - INTERVAL '28 days');
 
 
 /*For each hour and for each delivery location area, the total number of orders placed at that
 hour for that location area.*/
-
-/*area decided by dividing zipcode by 10000 to get sector code ie first 2 number of zipcode*/
 SELECT COUNT(ocid) FROM Order_List WHERE oorder_place_time - "TIMESTAMP" <= INTERVAL '1 hour' AND 
 ozipcode/10000 = "sector code"
+
 
 /*For each promotional campaign, the duration (in terms of the number of days/hours) of the
 campaign, and the average number of orders received during the promotion (i.e., the ratio of
 the total number of orders received during the campaign duration to the number of days/hours
 in the campaign duration).*/
 
-/* duration + average number of orders */
+/* returns tuple of duration, average number of orders */
 SELECT
   (p0.datetime_active_to - p0.pdatetime_active_from) AS duration,
   (
     SELECT
-      SUM(o.oid)
+      SUM(o.ocid)
     FROM
       Order_List o,
       Promotions p1
