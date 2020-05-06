9b75167dd98fb81a8dc3e05baf548717860c8dcd
Q-gabe <gabriel.ong@u.nus.edu>
2020-05-06 18:39:08 +0800

    add pages for Rider (preliminary)

---
+++ src/pages/rider/riderOrdersPage.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class riderOrdersPage extends Component {
+    render() {
+        return (
+            <div className="OrdersPage">
+                <center><h2>Orders page</h2></center>
+            </div>
+        )
+    }
+}
+
+export default riderOrdersPage
+
---
+++ src/pages/rider/riderStatsPage.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class riderStatsPage extends Component {
+    render() {
+        return (
+            <div className="statsPage">
+                <center><h2>Stats page</h2></center>
+            </div>
+        )
+    }
+}
+
+export default riderStatsPage
