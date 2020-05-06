3de42cbc10f7ff22dbf14d8a1d451d4e2421b11f
Q-gabe <gabriel.ong@u.nus.edu>
2020-05-06 18:38:03 +0800

    add pg module integration with Heroku DB

--- server/config/pool.js
+++ server/config/pool.js
@@ -1,15 +1,26 @@
 const { Pool } = require('pg');
 const keys = require('./keys');
 
+// Get current environment
+const env = process.env.NODE_ENV || 'development';
+
+// DEBUG
+env === 'development' ? console.log("Development environment detected.") 
+    : console.log("Production environment detected.")
+
+let connectionString = env === 'development' ? {
+    user: 'postgres',
+    password: keys.postgresPW,
+    host: 'localhost',
+    port: '5432',
+    database: 'mytestdb',
+    max: 10,
+    idleTimeoutMillis: 300,
+    connectionTimeoutMillis: 200
+} : {
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
+};
+
 // Create pool to communicate with database
-module.exports = new Pool({
-  // TODO: change to environment variable before deployment
-  user: 'postgres',
-  password: keys.postgresPW,
-  host: 'localhost',
-  port: '5432',
-  database: 'mytestdb',
-  max: 10,
-  idleTimeoutMillis: 300,
-  connectionTimeoutMillis: 200
-});
+module.exports = new Pool(connectionString);
